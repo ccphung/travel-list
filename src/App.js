8e5c726850934b8daea0d7537a1443aabe412281
Cédric Phung <cedric3011@outlook.fr>
2025-02-04 20:12:21 +0100

    correct function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Carger", quantity: 12, packed: false },
-];
-
 export default function App() {
   const [items, setItems] = useState([]);
 
@@ -14,7 +8,7 @@ export default function App() {
   }
 
   function handleDeleteItem(id) {
-    setItems([(items) => items.filter((item) => item.id !== id)]);
+    setItems((items) => items.filter((item) => item.id !== id));
   }
 
   return (
