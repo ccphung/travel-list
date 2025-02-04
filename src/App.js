c91ca656cac6918b95751ea1ccf7a3a992a88959
Cédric Phung <cedric3011@outlook.fr>
2025-02-04 10:03:55 +0100

    add compononents to App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 export default function App() {
-  function Logo() {
-    return <h1>🌴Far Away🧳</h1>;
-  }
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
 
-  function Form() {
-    return (
-      <div className="add-form">
-        <h3>What do you need for your 😘 trip ?</h3>
-      </div>
-    );
-  }
+function Logo() {
+  return <h1>🌴Far Away🧳</h1>;
+}
 
-  function PackingList() {
-    return <div className="list">LIST</div>;
-  }
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😘 trip ?</h3>
+    </div>
+  );
+}
 
-  function Stats() {
-    return (
-      <footer>
-        <em>🧳You have X items on your list, and you already packed X (X%)</em>
-      </footer>
-    );
-  }
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>🧳You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
